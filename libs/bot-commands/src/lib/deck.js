ded36d94003e5be87809705bf0c67e2ab592f1c6
Daniel McNelis <dwm253@gmail.com>
2023-02-13 18:14:29 -0500

    attach file to deck command

--- libs/bot-commands/src/lib/deck.js
+++ libs/bot-commands/src/lib/deck.js
@@ -73,8 +73,7 @@ export default {
 
         interaction.reply({ content: `Please check your DMs.` })
         const deckAttachments = await drawDeck(entry.ydk) || []
-        const ydkFile = new AttachmentBuilder(Buffer.from(entry.ydk), { name: `${player.discordName}#${player.discriminator}.ydk` })
-        console.log('ydkFile', ydkFile)
+        const ydkFile = new AttachmentBuilder(Buffer.from(entry.ydk), { name: `${player.discordName}#${player.discriminator}_${entry.tournament.abbreviation || entry.tournament.name}.ydk` })
         return interaction.member.send({ content: `${player.name}'s deck for ${entry.tournament.name} is:\n<${entry.url}>`, files: [...deckAttachments, ydkFile]}).catch((err) => console.log(err))
     }
 }
