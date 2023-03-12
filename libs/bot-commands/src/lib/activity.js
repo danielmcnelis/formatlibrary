265692c9d54bac4ac35bccd4ecf1fb80c55749b9
Daniel McNelis <dwm253@gmail.com>
2023-03-12 18:33:08 -0400

    activity comm

--- libs/bot-commands/src/lib/activity.js
+++ libs/bot-commands/src/lib/activity.js
@@ -26,99 +26,168 @@ export default {
             }
         })
 
-        if (!format) return await interaction.reply({ content: `Try using **/activity** in channels like: <#414575168174948372> or <#629464112749084673>.`})  
-        
-        const today = new Date()
-        const cutoff = new Date(
-            today.getMonth() === 11 ? today.getFullYear() : today.getFullYear() - 1,
-            today.getMonth() === 11 ? 1 : today.getMonth() + 1,
-            1
-        )
-
-        const matches = await Match.findAll({
-            where: { 
-                formatName: format.name,
-                createdAt: {
-                    [Op.gte]: cutoff
-                }
-            },
-            order: [['createdAt', 'ASC']]
-        })
-
-        const summary = {
-            '0': 0,
-            '1': 0,
-            '2': 0,
-            '3': 0,
-            '4': 0,
-            '5': 0,
-            '6': 0,
-            '7': 0,
-            '8': 0,
-            '9': 0,
-            '10': 0,
-            '11': 0,
-        }
-        
-        for (let i = 0; i < matches.length; i++) {
-            const match = matches[i]
-            const month = match.createdAt.getMonth()
-            summary[month] ? summary[month]++ : summary[month] = 1
-        }
-
-        const month = new Date().getMonth()
-        const months = [
-            "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-        ]
-
-        const labels = []
-        const data = []
-        for (let i = month + 1; i <= 11; i++) {
-            labels.push(months[i])
-            data.push(summary[i])
-        }
-        for (let i = 0; i <= month; i++) {
-            labels.push(months[i])
-            data.push(summary[i])
-        }
-        
-        const chart = new QuickChart()
-        chart.setConfig({
-            type: 'bar',
-            data: {
-                labels,
-                datasets: [
-                    {
-                        label: 'Activity',
-                        backgroundColor: 'rgb(10, 54, 138)',
-                        borderColor: 'rgb(10, 54, 138)',
-                        data: data,
-                        fill: false,
+        if (!format) {
+            const today = new Date()
+            const cutoff = new Date(
+                today.getMonth() === 11 ? today.getFullYear() : today.getFullYear() - 1,
+                today.getMonth() === 11 ? 1 : today.getMonth() + 1,
+                1
+            )
+    
+            const matches = await Match.findAll({
+                where: { 
+                    createdAt: {
+                        [Op.gte]: cutoff
                     }
-                ],
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: `${format.name} Activity Chart`,
-                },
-                legend: {
-                    display: false
-                },
-                scales: {
-                    yAxes: [
-                        {
-                            ticks: {
-                                suggestedMax: 100
-                            }
-                        }
-                    ]
-                },
-            },
-        })
+                }
+            })
+    
+            const summary = {}
+            
+            for (let i = 0; i < matches.length; i++) {
+                const match = matches[i]
+                summary[match.formatName] ? summary[match.formatName]++ : summary[match.formatName] = 1
+            }
+    
+            const labels = []
+            const data = []
 
-        const url = await chart.getShortUrl()
-        return await interaction.reply({ content: `${url}` })
+            for (let [key, value] in Object.entries(summary)) {
+                if (value < 10) continue
+                labels.push(key)
+                data.push(value)
+            }
+
+            const chart = new QuickChart()
+            chart.setConfig({
+                type: 'bar',
+                data: {
+                    labels,
+                    datasets: [
+                        {
+                            label: 'Activity',
+                            backgroundColor: 'rgb(10, 54, 138)',
+                            borderColor: 'rgb(10, 54, 138)',
+                            data: data,
+                            fill: false,
+                        }
+                    ],
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: `Format Library Activity Chart`,
+                    },
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [
+                            {
+                                ticks: {
+                                    suggestedMax: 100
+                                }
+                            }
+                        ]
+                    },
+                },
+            })
+    
+            const url = await chart.getShortUrl()
+            return await interaction.reply({ content: `${url}` })
+        } else {
+            const today = new Date()
+            const cutoff = new Date(
+                today.getMonth() === 11 ? today.getFullYear() : today.getFullYear() - 1,
+                today.getMonth() === 11 ? 1 : today.getMonth() + 1,
+                1
+            )
+    
+            const matches = await Match.findAll({
+                where: { 
+                    formatName: format.name,
+                    createdAt: {
+                        [Op.gte]: cutoff
+                    }
+                },
+                order: [['createdAt', 'ASC']]
+            })
+    
+            const summary = {
+                '0': 0,
+                '1': 0,
+                '2': 0,
+                '3': 0,
+                '4': 0,
+                '5': 0,
+                '6': 0,
+                '7': 0,
+                '8': 0,
+                '9': 0,
+                '10': 0,
+                '11': 0,
+            }
+            
+            for (let i = 0; i < matches.length; i++) {
+                const match = matches[i]
+                const month = match.createdAt.getMonth()
+                summary[month] ? summary[month]++ : summary[month] = 1
+            }
+    
+            const month = new Date().getMonth()
+            const months = [
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            ]
+    
+            const labels = []
+            const data = []
+            for (let i = month + 1; i <= 11; i++) {
+                labels.push(months[i])
+                data.push(summary[i])
+            }
+            for (let i = 0; i <= month; i++) {
+                labels.push(months[i])
+                data.push(summary[i])
+            }
+            
+            const chart = new QuickChart()
+            chart.setConfig({
+                type: 'bar',
+                data: {
+                    labels,
+                    datasets: [
+                        {
+                            label: 'Activity',
+                            backgroundColor: 'rgb(10, 54, 138)',
+                            borderColor: 'rgb(10, 54, 138)',
+                            data: data,
+                            fill: false,
+                        }
+                    ],
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: `${format.name} Activity Chart`,
+                    },
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [
+                            {
+                                ticks: {
+                                    suggestedMax: 100
+                                }
+                            }
+                        ]
+                    },
+                },
+            })
+    
+            const url = await chart.getShortUrl()
+            return await interaction.reply({ content: `${url}` })
+        }
     }
 }
