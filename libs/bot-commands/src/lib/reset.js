7be89aa6b93de7be6f85ede8f0b5b15c146ab1c0
Daniel McNelis <dwm253@gmail.com>
2023-03-17 01:01:14 -0400

    trivia bug fixes

--- libs/bot-commands/src/lib/reset.js
+++ libs/bot-commands/src/lib/reset.js
@@ -1,6 +1,6 @@
 
 import { SlashCommandBuilder } from 'discord.js'    
-import { hasFullAccess } from '@fl/bot-functions'
+import { isMod, hasFullAccess } from '@fl/bot-functions'
 import { emojis } from '@fl/bot-emojis'
 import { Server, TriviaEntry } from '@fl/models'
 
@@ -9,12 +9,14 @@ export default {
 		.setName('reset')
 		.setDescription('Reset trivia! 🤔'),
 	async execute(interaction) {
+        await interaction.deferReply()
         const server = !interaction.guildId ? {} : 
             await Server.findOne({ where: { id: interaction.guildId }}) || 
             await Server.create({ id: interaction.guildId, name: interaction.guild.name })
         
-        if (!hasFullAccess(server)) return await interaction.reply({ content: `This feature is only available in Format Library. ${emojis.FL}`})
-        if (interaction.channel.id !== '1085316454053838981') return await interaction.reply({ content: `Try using **/trivia** in the <#1085316454053838981> channel. 📚 🐛`})
+        if (!hasFullAccess(server)) return await interaction.editReply({ content: `This feature is only available in Format Library. ${emojis.FL}`})
+        if (!isMod(server, interaction.member)) return await interaction.editReply({ content: `You do not have permission to do that.`})
+        if (interaction.channel.id !== '1085316454053838981') return await interaction.editReply({ content: `Try using **/trivia** in the <#1085316454053838981> channel. 📚 🐛`})
 
         const triviaEntries = await TriviaEntry.findAll()
         for (let i = 0; i < triviaEntries; i++) {
@@ -22,6 +24,6 @@ export default {
             await entry.destroy()
         }
 
-        return interaction.reply({ content: `The Trivia game has been reset. ${emojis.mlady}`})
+        return interaction.editReply({ content: `The Trivia game has been reset. ${emojis.mlady}`})
 	}
 }
