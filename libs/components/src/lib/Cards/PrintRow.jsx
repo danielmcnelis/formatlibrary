db11e1751377a83293eab80ece564aa477ef1099
Daniel McNelis <dwm253@gmail.com>
2023-03-31 11:19:28 -0400

    rulings

--- libs/components/src/lib/Cards/PrintRow.jsx
+++ libs/components/src/lib/Cards/PrintRow.jsx
@@ -9,6 +9,7 @@ export const PrintRow = (props) => {
   const openNewTab = () => window.open(tcgPlayerUrl, "_blank")
   const id = print.rarity === '10000 Secret Rare' ? 'tenThousandSecretRare' : camelize(print.rarity)
   const prices = [print.unlimPrice, print.firstPrice, print.limPrice].filter((e) => !!e)
+  const minPrice = Math.min(...prices) ? '$' + Math.min(...prices) : 'N/A'
 
   return (
     <tr onClick={() => openNewTab()} className={`${evenOrOdd}-print-row`}>
@@ -31,7 +32,7 @@ export const PrintRow = (props) => {
           </div>
         </td>
         <td className="print-cell-6">
-            {'$' + Math.min(...prices)}
+            {minPrice}
         </td>
     </tr>
   )
