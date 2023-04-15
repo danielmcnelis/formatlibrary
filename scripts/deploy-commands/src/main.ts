50e33bc8ec87d24b8639c49749826b9141b76514
Daniel McNelis <dwm253@gmail.com>
2023-04-14 22:38:37 -0400

    guild/global application commands

--- scripts/deploy-commands/src/main.ts
+++ scripts/deploy-commands/src/main.ts
@@ -1,24 +1,14 @@
 
 import { REST, Routes } from 'discord.js'
-// import * as fs from 'fs'
 import { config } from '@fl/config'
 import commands from '@fl/bot-commands'
 
 const discordBotToken = config.services.bot.token
 const clientId = config.services.bot.clientId
+const guildId = '414551319031054346'
 
-// const guildId = '414551319031054346'
-// Grab all the command files from the commands directory you created earlier
-// const commandFiles = fs.readdirSync('./services/bot/src/app/commands').filter(file => file.endsWith('.js'))
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-// for (const file of commandFiles) {
-//     if (file.includes('index')) continue
-// 	const command = require(`./services/bot/src/app/commands/${file}`)
-// 	commands.push(command.data.toJSON())
-// }
-
-const commandNames = Object.values(commands).map((command: any) => command.data.toJSON())
+const formatLibraryCommandNames = Object.values(commands.formatLibraryCommands).map((command: any) => command.data.toJSON())
+const globalCommandNames = Object.values(commands.globalCommands).map((command: any) => command.data.toJSON())
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(discordBotToken);
@@ -26,15 +16,25 @@ const rest = new REST({ version: '10' }).setToken(discordBotToken);
 // and deploy your commands!
 ;(async () => {
 	try {
-		console.log(`Started refreshing ${commandNames.length} application (/) commands.`)
+		console.log(`Started refreshing ${formatLibraryCommandNames.length} Format Library guild (/) commands.`)
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: formatLibraryCommandNames },
+		)
+
+		console.log(`Successfully reloaded ${formatLibraryCommandNames.length} Format Library guild (/) commands.`)
+
+		console.log(`Started refreshing ${globalCommandNames.length} global application (/) commands.`)
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		await rest.put(
             Routes.applicationCommands(clientId),
-            { body: commandNames },
-		);
+            { body: globalCommandNames },
+		)
 
-		console.log(`Successfully reloaded ${commandNames.length} application (/) commands.`);
+		console.log(`Successfully reloaded ${globalCommandNames.length} global application (/) commands.`)
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error)
