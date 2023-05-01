e74a4e046702ee10191917532e269cb2f17f55fe
Daniel McNelis <dwm253@gmail.com>
2023-05-01 18:18:37 -0400

    premium and subscriptions

--- apps/formatlibrary/src/app/App.tsx
+++ apps/formatlibrary/src/app/App.tsx
@@ -7,30 +7,43 @@ const playerId = getCookie('playerId')
 
 const App = () => {
     const [isSubscriber, setIsSubscriber] = useState(false)
+    const [checkedSubscription, setCheckedSubscription] = useState(false)
     console.log('isSubscriber', isSubscriber)
 
     // USE EFFECT
     useEffect(() => {
-        const checkIfSubscriber = async () => {
-            try {
-                const { status } = await axios.get(`/api/players/subscriber/${playerId}`)
-                if (status === 200) setIsSubscriber(true)
-            } catch (err) {
-                console.log(err)
+        if (playerId) {
+            const checkIfSubscriber = async () => {
+                try {
+                    const { status } = await axios.get(`/api/players/subscriber/${playerId}`)
+                    if (status === 200) setIsSubscriber(true)
+                } catch (err) {
+                    console.log(err)
+                }
+    
+                setCheckedSubscription(true)
             }
+    
+            checkIfSubscriber()
         }
-
-        checkIfSubscriber()
     }, [])
 
   return (
     <div className="app">
         {
-            !isSubscriber ? (
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2048547741313745" crossOrigin="anonymous"></script>
-            ) : ''
+            playerId && !checkedSubscription ? (
+                <div></div>
+            ) : (
+                <div>
+                    {
+                        !isSubscriber ? (
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2048547741313745" crossOrigin="anonymous"></script>
+                        ) : ''
+                    }
+                    <Router />
+                </div>
+            )
         }
-      <Router />
     </div>
   )
 }
