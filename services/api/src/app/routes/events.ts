c1974129d1d80c43308f3e4637e898cf77083f12
Daniel McNelis <dwm253@gmail.com>
2023-05-04 01:13:02 -0400

    event gallery

--- services/api/src/app/routes/events.ts
+++ services/api/src/app/routes/events.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { getEvents, countEvents, eventsRecent, eventsId, eventsCreate, eventsCommunity } from '../middleware'
+import { getEvents, countEvents, eventsRecent, eventsId, eventsCreate, eventsCommunity, eventsGallery } from '../middleware'
 
 const router = Router()
 
 router.get('/api/events/recent/:format', eventsRecent)
+
+router.get('/api/events/gallery/:format', eventsGallery)
 
 router.get('/api/events/community/:community', eventsCommunity)
 
