40f10fce32d74ce2cc7dd1a1fca623781968ea2a
Daniel McNelis <dwm253@gmail.com>
2023-05-05 01:08:51 -0400

    statuses

--- libs/bot-commands/src/lib/help.js
+++ libs/bot-commands/src/lib/help.js
@@ -15,13 +15,13 @@ export default {
             .setThumbnail('https://cdn.formatlibrary.com/images/logos/Format%20Library.png')
             .addFields(
                 { name: 'How to Use This Bot', value: '\nThe following commands can be used for any format in the appropriate server, channels, or via DM.' },
-        	    { name: 'Cards', value: '\n/search - Look up a card. \n'},
-                { name: 'Rated Pools', value: '\n/rated - DM to join a rated pool. \n/exit - DM to leave a rated pool. \n/pools - DM to view your rated pools. \n'},
-                { name: 'Ranked Play', value: '\n/stats - (blank or @user) - Post a player\'s stats. \n/loss - (@user) - Report a loss to another player. \n/top - (n) - Post the top rated players (100 max). \n/h2h - (@user + @user) - Post the H2H record between 2 players. \n/role - Add or remove a ranked play role. \n/undo - Undo the last report if you made a mistake. \n/hist - Post a graph of your Elo history. \n/act - Post a graph of dueling activity. \n'},
-                { name: 'Format Info', value: '\n/legal - Privately check if your deck is legal. \n/list - View the Forbidden and Limited list. \n'},
+        	    { name: 'Cards', value: '\n/card - Look up a card. \n'},
+                { name: 'Rated Pools', value: '\n/rated - DM to join a rated pool. \n/exit - DM to leave a rated pool. \n/pools - DM to view your rated pools. \n/lobby - Post a list of recent rated pool activity. \n'},
+                { name: 'Rated Play', value: '\n/stats - Post a player\'s stats. \n/loss - Report a loss to another player. \n/leaderboard - Post the top rated players. \n/h2h - Post the H2H record between 2 players. \n/role - Add or remove a ranked play role. \n/undo - Undo the last report if you made a mistake. \n/history - Post a graph of your Elo history. \n/records - View your recent rated match records. \n/activity - Post a graph of dueling activity. \n'},
+                { name: 'Format Info', value: '\n/legal - Privately check if your deck is legal. \n/info - Post a link to the format overview. \n'},
                 { name: 'Tournaments', value: '\n/join - Register for an upcoming tournament.\n/resubmit - Resubmit your deck list for a tournament. \n/drop - Drop from a tournament. \n/bracket - Post the bracket(s) for the current tournament(s).'},
-                { name: 'Ironmans', value: '\n/iron - Join an ironman queue.\n/q - Check an ironman queue. \n'},
-                { name: 'Miscellaneous', value: '\n/db - (name or @user) - Set or check a DuelingBook name. \n/prof - (blank or @user) - Post a player\'s profile. \n/medals - (blank or @user) - Post a player\'s best medals. \n/rng (X) - Random number from 1 to X. \n/flip - Flip a coin. \n/dice - Roll a 6-sided die. \n/bot - View the RetroBot User Guide. \n/mod - View the RetroBot Moderator Guide.'}
+                // { name: 'Ironmans', value: '\n/iron - Join an ironman queue.\n/queue - Check an ironman queue. \n'},
+                { name: 'Misc.', value: '\n/duelingbook - Set your DuelingBook name. \n/profile - Post a player\'s profile. \n/rng - Random number from 1 to X. \n/flip - Flip a coin. \n/dice - Roll a 6-sided die. \n/help - View the RetroBot User Guide. \n/mod - View the RetroBot Moderator Guide.'}
             )
             
         interaction.user.send({ embeds: [botEmbed] }).catch((err) => console.log(err))
--- libs/bot-commands/src/lib/mod.js
+++ libs/bot-commands/src/lib/mod.js
@@ -19,13 +19,13 @@ export default {
         const botEmbed = new EmbedBuilder()
 	        .setColor('#38C368')
         	.setTitle('RetroBot Moderator Guide')
-	        .setDescription('A Rankings and Tournament Bot powered by FormatLibrary.com.\n' )
+	        .setDescription('A Yu-Gi-Oh! Bot powered by FormatLibrary.com.\n' )
 	        .setURL('https://discord.com/invite/formatlibrary')
 	        .setAuthor({ name: 'Jazz#2704', iconURL: 'https://i.imgur.com/wz5TqmR.png', url: 'https://discord.com/invite/formatlibrary' })
             .setThumbnail('https://cdn.formatlibrary.com/images/logos/Format%20Library.png')
         	.addFields(
-                { name: 'Ranked Play', value: '\n!manual - (@winner + @loser) - Manually record a match result. \n!undo - Undo the most recent loss, even if you did not report it.'},
-                { name: 'Tournaments', value: '\n!create - (tournament name) - Create a new tournament.  \n!signup - (@user) - Directly add a player to a bracket. \n!noshow - (@user) - Report a no-show. \n!deck - (@user) - Check a player\'s tournament deck. \n!remove - (@user) - Remove a player from a bracket. \n!start - Start a tournament. \n!end - (tournament name) - End a tournament. \n!timer - (x) - Start a timer for X minutes.'},
+                { name: 'Rated Play', value: '\n/manual - Manually record a match result. \n/records - View a player\'s recent match records. \n/undo - Undo one of the recent match results.'},
+                { name: 'Tournaments', value: '\n/create -  Create a new tournament \n/destroy -  Delete a tournament. \n/signup - Directly add a player to a bracket. \n/close - Close tournament registration. \n/open - Open tournament registration. \n/start - Start a tournament. \n/settimer- Start a timer and notify players. \n/noshow - Report a no-show. \n/deck - Check a player\'s tournament deck. \n/remove - Remove a player from a bracket.  \n/end - End a tournament.'},
             )
 
         interaction.user.send({ embeds: [botEmbed] }).catch((err) => console.log(err))
