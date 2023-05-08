5047bcba9250ba1b92b70d53e0e4749231d5673a
Daniel McNelis <dwm253@gmail.com>
2023-05-08 16:01:58 -0400

    bot updates

--- libs/bot-commands/src/lib/flair.js
+++ libs/bot-commands/src/lib/flair.js
@@ -5,7 +5,7 @@ export default {
     data: new SlashCommandBuilder()
         .setName('flair')
         .setDescription(`Add or remove a Subscriber Role. 👒`)
-        .addRoleOption(option =>
+        .addStringOption(option =>
             option
                 .setName('role')
                 .setDescription('Subscriber Role.')
@@ -18,16 +18,15 @@ export default {
         ,
     async execute(interaction) {
         console.log('interaction.options', interaction.options)
-        const role = interaction.options.getRole('role')
-        console.log('role', role)
+        const roleId = interaction.options.getString('role')
 
         try {
-            await interaction.member.roles.add(role.id)
+            await interaction.member.roles.add(roleId)
         } catch (err) {
             console.log(err)
-            return await interaction.reply({ content: `Error: Unable to add ${role.name} role.`})
+            return await interaction.reply({ content: `Error: Unable to add role.`})
         }
         
-        return await interaction.reply({ content: `You now have the ${role.name} role.`})
+        return await interaction.reply({ content: `You now have the role.`})
     }
 }
