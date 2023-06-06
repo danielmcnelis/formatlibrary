8711ece5811cee9a25510e1ab712b76ef3199c36
Daniel McNelis <dwm253@gmail.com>
2023-06-06 00:55:33 -0400

    one piece

--- libs/bot-commands/src/lib/replay.js
+++ libs/bot-commands/src/lib/replay.js
@@ -75,7 +75,7 @@ export default {
             }
         })
 
-        const challongeMatch = await axios.get(`https://api.challonge.com/v1/tournaments/{tournament}/matches/${match.challongeMatchId}.json?api_key=${server.challongeAPIKey}`).catch((err) => console.log(err))
+        const challongeMatch = await axios.get(`https://api.challonge.com/v1/tournaments/${tournament.id}/matches/${match.challongeMatchId}.json?api_key=${server.challongeAPIKey}`).catch((err) => console.log(err))
         if (!match) return await interaction.editReply({ content: `Error: Match report not found.`})	
         if (!challongeMatch) return await interaction.editReply({ content: `Error: Challonge match not found.`})	
         const replay = await Replay.findOne({ where: { matchId: match.id }})
