35fd5313c8e0b2612bc22302c0ca1dbcc1e161ab
Daniel McNelis <dwm253@gmail.com>
2023-06-07 19:30:15 -0400

    replay comm

--- libs/bot-commands/src/lib/replay.js
+++ libs/bot-commands/src/lib/replay.js
@@ -36,7 +36,7 @@ export default {
     async execute(interaction) {
         await interaction.deferReply()
         const url = interaction.options.getString('url')
-        const input = interaction.options.getString('tournament')?.replace(/[\s_-]/g, '')
+        const input = interaction.options.getString('tournament')?.replace(/[\s-]/g, '')
         const winner = interaction.options.getUser('winner')
         const loser = interaction.options.getUser('loser')
         const server = await Server.findOrCreateByIdOrName(interaction.guildId, interaction.guild?.name)
