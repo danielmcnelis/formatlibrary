fae40dcff2dcff8e5c907496a838c8a000da5866
Daniel McNelis <dwm253@gmail.com>
2023-06-08 00:51:19 -0400

    matchups and subscriber perks

--- libs/components/src/lib/Decks/MatchupBar.jsx
+++ libs/components/src/lib/Decks/MatchupBar.jsx
@@ -8,15 +8,17 @@ export const Matchup = (props) => {
     if (!deckType) return <p></p>
     console.log('deckType')
     const fraction = wins / total
+    console.log('fraction', fraction)
 
     const backgroundColor = fraction < 0.4 ? '#cc0000' :
-        fraction >= 0.4 & fraction < 0.45  ? 'e06666' :
-        fraction >= 0.45 & fraction < 0.5  ? 'ea9999' :
+        fraction >= 0.4 && fraction < 0.45  ? 'e06666' :
+        fraction >= 0.45 && fraction < 0.5  ? 'ea9999' :
         fraction === 0.5 ? '#eeeeee' :
-        fraction >= 0.5 & fraction < 0.55  ? '#b6d7a8' :
-        fraction >= 0.55 & fraction < 0.6  ? '#93c47d' :
+        fraction > 0.5 && fraction < 0.55  ? '#b6d7a8' :
+        fraction >= 0.55 && fraction < 0.6  ? '#93c47d' :
         '#6aa84f'
-  
+        console.log('backgroundColor', backgroundColor)
+
       return (
           <Link to={`/decktypes/${deckType}?format=${format}`} key={deckType} className="matchup-cell" style={{backgroundColor}}>
             <p>{wins}-{losses} ({(fraction * 100).toFixed(1)}%) vs {capitalize(deckType)}</p>
