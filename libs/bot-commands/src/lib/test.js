e54de6577150f42141f82743d6d4c3f2f54444af
Daniel McNelis <dwm253@gmail.com>
2023-06-09 20:46:56 -0400

    test and trivia

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -3,8 +3,7 @@ import { SlashCommandBuilder } from 'discord.js'
 import { isProgrammer, updateSets, downloadNewCards, updateMarketPrices } from '@fl/bot-functions'
 import { emojis } from '@fl/bot-emojis'
 // import { client } from '../client'
-import {util} from 'util'
-console.log('util')
+import util from 'util'
 
 export default {
     data: new SlashCommandBuilder()
