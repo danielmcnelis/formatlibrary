656474f2542c54c035939f910e417b9fcef75b47
Daniel McNelis <dwm253@gmail.com>
2023-06-09 20:42:46 -0400

    test and trivia

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -3,6 +3,7 @@ import { SlashCommandBuilder } from 'discord.js'
 import { isProgrammer, updateSets, downloadNewCards, updateMarketPrices } from '@fl/bot-functions'
 import { emojis } from '@fl/bot-emojis'
 // import { client } from '../client'
+import util from 'util'
 
 export default {
     data: new SlashCommandBuilder()
@@ -13,12 +14,15 @@ export default {
             // console.log('interaction.member', interaction.member)
             const member = await interaction.guild.members.fetch(interaction.member.user.id)
             // console.log('member', member)
+            console.log('member.user', member.user)
+            console.log('member.user.display_name', member.user.display_name)
             console.log('member.user.displayName', member.user.displayName)
             console.log('member.user.globalName', member.user.globalName)
+            console.log('member.user.global_name', member.user.global_name)
             console.log('member.user.nickname', member.user.nickname)
-            console.log('member.displayName', member.displayName)
-            console.log('member.globalName', member.globalName)
-            console.log('member.nickname', member.nickname)
+
+            console.log(util.inspect(member.user, {showHidden: false, depth: null, colors: true}))
+            
             await interaction.reply(emojis.yellow)
         } else {
             await interaction.reply('🧪')
