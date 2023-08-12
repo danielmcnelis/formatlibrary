883564f726bdd9d782277a2de9708d443a2c87e5
Daniel McNelis <dwm253@gmail.com>
2023-08-12 16:54:32 -0400

    globalname

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -5,6 +5,7 @@ import { emojis } from '@fl/bot-emojis'
 import { client } from '../client'
 import { Tournament, Server } from '@fl/models'
 import axios from 'axios'
+import parse from 'html-react-parser'
 // import { config } from '@fl/config'
 
 export default {
@@ -16,6 +17,10 @@ export default {
         if (isProgrammer(interaction.member)) {
             const {data} = await axios.get('https://www.duelingbook.com/replay?id=5213728')
             console.log('data', data)
+            const html = parse(data)
+            console.log('html', html)
+            console.log('html.player1', html.player1)
+            console.log('html.player2', html.player2)
             await interaction.editReply(emojis.yellow)
         } else {
             await interaction.editReply('🧪')
