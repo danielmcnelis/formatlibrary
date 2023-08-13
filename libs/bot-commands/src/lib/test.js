f77faae3c94ca6765d8727c29e9141bf84e688d6
Daniel McNelis <dwm253@gmail.com>
2023-08-12 21:49:35 -0400

    css

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -1,9 +1,9 @@
 
 import { SlashCommandBuilder } from 'discord.js'
-import { getMatches, getParticipants, calculateStandings, autoRegisterTopCut, isProgrammer, conductCensus, updateSets, downloadNewCards, updateMarketPrices } from '@fl/bot-functions'
+import { shuffleArray, getMatches, getParticipants, calculateStandings, autoRegisterTopCut, isProgrammer, conductCensus, updateSets, downloadNewCards, updateMarketPrices } from '@fl/bot-functions'
 import { emojis } from '@fl/bot-emojis'
 import { client } from '../client'
-import { Tournament, Server } from '@fl/models'
+import { Tournament, Server, TriviaQuestion } from '@fl/models'
 import axios from 'axios'
 // import { config } from '@fl/config'
 
@@ -14,6 +14,19 @@ export default {
     async execute(interaction) {
         await interaction.deferReply()
         if (isProgrammer(interaction.member)) {
+            console.log(`all questions have been asked!`)
+            let askedQuestions = await TriviaQuestion.findAll()
+
+            const shuffledQuestions = shuffleArray(askedQuestions)
+
+            for (let i = 0; i < shuffledQuestions.length; i++) {
+                const q = shuffledQuestions[i]
+                await q.update({
+                    order: i + 1,
+                    askedRecently: false
+                })
+            }
+
             await interaction.editReply(emojis.yellow)
         } else {
             await interaction.editReply('🧪')
