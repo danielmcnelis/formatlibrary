2bb8450ce78e0e5d991f1aeb86f51bca9d21313b
Daniel McNelis <dwm253@gmail.com>
2023-08-30 13:31:55 -0400

    replay coverage

--- services/api/src/app/middleware/events.ts
+++ services/api/src/app/middleware/events.ts
@@ -246,7 +246,7 @@ export const eventsId = async (req, res, next) => {
             eventId: event.id
         },
         include: [{ model: Player, as: 'loser' }, { model: Player, as: 'winner' }],
-        order: [['display', 'DESC'], ['round', 'DESC']]
+        order: [['display', 'DESC'], ['suggestedOrder', 'DESC']]
     })
 
     const topDecks = await Deck.findAll({
