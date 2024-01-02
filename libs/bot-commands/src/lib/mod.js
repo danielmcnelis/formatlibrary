fb66b550bb7b05ebf8ec8e5fc95c91cdf363c23f
Daniel McNelis <dwm253@gmail.com>
2024-01-02 14:55:00 -0500

    handle swiss

--- libs/bot-commands/src/lib/mod.js
+++ libs/bot-commands/src/lib/mod.js
@@ -24,7 +24,7 @@ export default {
             .setThumbnail('https://cdn.formatlibrary.com/images/logos/Format%20Library.png')
         	.addFields(
                 { name: 'Rated Play', value: '\n/manual - Manually record a match result. \n/records - View a player\'s match records. \n/undo - Undo one of the recent match results.'},
-                { name: 'Tournament Creation', value: '\n/create -  Create a new tournament. \n/destroy -  Delete a tournament. \n/start - Start a tournament. \n/end - End a tournament.'},
+                { name: 'Tournament Creation', value: '\n/create -  Create a new tournament. \n/destroy -  Delete a tournament. \n/start - Start a tournament. \n/end - End a tournament. \n/points - Edit points per Win/Tie/Bye. \n/tiebreakers - Edit tiebreakers (Swiss only).'},
                 { name: 'Tournament Registration', value: '\n/signup - Directly add a player to a bracket. \n/remove - Remove a player from a bracket. \n/close - Close tournament registration. \n/open - Open tournament registration.'},
                 { name: 'Tournament Hosting', value: '\n/settimer - Start a timer and notify players. \n/noshow - Report a no-show. \n/deck - Check a player\'s tournament deck.'},
             )
