3d3ca88ceed4d494efc5c9acc5842ef1a6884d49
Daniel McNelis <dwm253@gmail.com>
2024-01-02 17:42:21 -0500

    ads an proxies

--- services/api/src/app/middleware/cookies.ts
+++ services/api/src/app/middleware/cookies.ts
@@ -2,7 +2,7 @@
 // TRACK VISIT
 export const trackVisit = async (req, res, next) => {
     try {
-        console.log('tracking??????')
+        console.log('tracking?????? .')
         res.cookie('visited', Date.now(), {
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
