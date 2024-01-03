659189bc71ba50eb78120d2629b10097094c449f
Daniel McNelis <dwm253@gmail.com>
2024-01-02 19:23:42 -0500

    ads an proxies

--- apps/formatlibrary/src/app/App.tsx
+++ apps/formatlibrary/src/app/App.tsx
@@ -11,13 +11,21 @@ const socket = io(config.siteUrl)
 
 const App = () => {
     const playerId = getCookie('playerId')
+    console.log('playerId', playerId)
     const visited = getCookie('visited')
+    console.log('visited', visited)
     const [isSubscriber, setIsSubscriber] = useState(false)
+    console.log('isSubscriber', isSubscriber)
     const [isTracking, setIsTracking] = useState(false)  
+    console.log('isTracking', isTracking)
     const [checkedTracking, setCheckedTracking] = useState(false)  
+    console.log('checkedTracking', checkedTracking)
     const [checkedSubscription, setCheckedSubscription] = useState(false)  
+    console.log('checkedSubscription', checkedSubscription)
     const adBlockDetected = useDetectAdBlock()
+    console.log('adBlockDetected', adBlockDetected)
     const [showReminder, setShowReminder] = useState(false) 
+    console.log('showReminder', showReminder)
 
     // USE EFFECT
     useEffect(() => {
@@ -70,15 +78,28 @@ const App = () => {
         <div className="app">
             {
                 playerId && !checkedSubscription ? (<SocketProvider value={socket}> <Router/> </SocketProvider>) :
-                    adBlockDetected && checkedSubscription && !isSubscriber && showReminder ? (
+                    adBlockDetected && showReminder ? (
                         <div className="ad-block-detected">
-                            <h2>Reminder: Please allow cookies and disable your ad-blocker to view this website.</h2>
-                            <p>Format Library depends on modest ad revenue to operate and produce content. If you do not wish to see ads, you can also subscribe to the <a style={{color: 'blue'}} href="https://discord.com/invite/formatlibrary">Format Library Discord server</a> for $3.99/month and log-in below:</p>
+                            <h2>Reminder: Please allow cookies and disable your ad-blocker.</h2>
+                            <p>Format Library depends on modest ad revenue to operate and produce content. <span role="img" aria-label="artist">🧑‍🎨</span></p>
+                            <p>We kindly ask you to whitelist us on your ad-blocker. <span role="img" aria-label="smiley">😊</span></p>
+                            <br/>
+                            <p>You can also subscribe to the <a style={{color: 'blue'}} href="https://discord.com/invite/formatlibrary">Format Library Discord server</a> Supporter Tier for $3.99/month and log-in to the website below.</p>
+                            <div>
+                                <ul className="perks">
+                                    <h3>Supporter perks include:</h3>
+                                    <li>Ad-free browsing</li>
+                                    <li>Matchup data</li>
+                                    <li>Complete tournament coverage</li>
+                                    <li>Special Discord badges</li>
+                                </ul>
+                            </div>
+                            <br/>
                             <div className="horizontal-centered-flexbox">
-                                <a href="/auth/login/">
+                                <a className="show-cursor reminder-button" href="/auth/login/">
                                     <h1 className="login">LOGIN</h1>
                                 </a>
-                                <div className="show-cursor" onClick={() => window.location.reload()}>
+                                <div className="show-cursor reminder-button" onClick={() => window.location.reload()}>
                                     <h1 className="login">CONTINUE</h1>
                                 </div>
                             </div>
