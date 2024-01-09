a49d870b65273835590f16e62dc66a2e80bef16d
Daniel McNelis <dwm253@gmail.com>
2024-01-09 17:58:58 -0500

    ads an proxies

--- libs/bot-functions/src/lib/tournament.js
+++ libs/bot-functions/src/lib/tournament.js
@@ -302,7 +302,7 @@ export const sendDeck = async (interaction, id) => {
         const isAuthor = interaction.user.id === deck.player.discordId
         deckAttachments.forEach((attachment, index) => {
             if (index === 0) {
-                interaction.member.send({ content: `${isAuthor ? `${deck.player.globalName || deck.player.discordName}'s` : 'Your'} deck for ${deck.tournament.name} is:`, files: [attachment] }).catch((err) => console.log(err))
+                interaction.member.send({ content: `${isAuthor ? `${deck.player?.globalName || deck.player?.discordName}'s` : 'Your'} deck for ${deck.event?.name} is:`, files: [attachment] }).catch((err) => console.log(err))
             } else {
                 interaction.member.send({ files: [attachment] }).catch((err) => console.log(err))
             }
