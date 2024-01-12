6edc8fa0917ae7a68d17fb7e824c2c8c46e6a325
Daniel McNelis <dwm253@gmail.com>
2024-01-11 23:21:23 -0500

    recording challonge matches

--- libs/bot-commands/src/lib/fix.js
+++ libs/bot-commands/src/lib/fix.js
@@ -1,64 +1,125 @@
 
 import { SlashCommandBuilder } from 'discord.js'
 import { isProgrammer } from '@fl/bot-functions'
-import { Deck, DeckType, Event, Format, Match, Matchup, Player, Replay, Server, Tournament } from '@fl/models'
+import { Alius, Deck, DeckType, Event, Format, Match, Matchup, Player, Replay, Server, Tournament } from '@fl/models'
 import { calculateStandings, generateMatchupData, fixPlacements } from '@fl/bot-functions'
 import { Op } from 'sequelize'
 import axios from 'axios'
+import promptSync from 'prompt-sync'
 
 export default {
     data: new SlashCommandBuilder()
         .setName('fix')
         .setDescription('Admin Only - Fix an issue. 🛠️')
-		// .addStringOption(str =>
-        //     str
-        //         .setName('tournament')
-        //         .setDescription('Enter tournament name or abbreviation.')
-        //         .setRequired(true)
-        // )
+		.addStringOption(str =>
+            str
+                .setName('tournament')
+                .setDescription('Enter tournament name or abbreviation.')
+                .setRequired(true)
+        )
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.deferReply()
         if (isProgrammer(interaction.member)) {
             const server = await Server.findOrCreateByIdOrName(interaction.guildId, interaction.guild?.name)
-            // const [input, topCut] = interaction.options.getString('tournament').split('_')
-            const replays = await Replay.findAll({ 
-                where: { 
-                    suggestedOrder: {[Op.or]: [null, 0]},
-                    matchId: {[Op.not]: null}
-                },
-                include: Match
-            })
+            const tournamentId = interaction.options.getString('tournament')
+            const prompt = promptSync()
+            const { data: tournamentData } = await axios.get(`https://api.challonge.com/v1/tournaments/${tournamentId}.json?api_key=${server.challongeAPIKey}`)
+            console.log('!!tournamentData', !!tournamentData)
+            let tournament
+            const count = await Tournament.count({ id: tournamentData.tournament.id.toString()})
+            if (!count) {
+                tournament = await Tournament.create({
+                    id: tournamentData.tournament.id.toString(),
+                    name: tournamentData.tournament.name,
+                    abbreviation: tournamentData.tournament.name,
+                    url: tournamentData.tournament.url,
+                    type: tournamentData.tournament.tournament_type,
+                    formatName: 'Goat',
+                    formatId: 8,
+                    createdAt: tournamentData.tournament.created_at,
+                    community: 'GoatFormat.com',
+                    serverId: '459826576536764426',
+                    state: 'complete'
+                })
 
-            const data = {}
+                console.log('!!tournament', !!tournament)
+            }
+        
+            const { data: participants } = await axios.get(`https://api.challonge.com/v1/tournaments/${tournamentId}/participants.json?api_key=${server.challongeAPIKey}`)
+            const { data: matches } = await axios.get(`https://api.challonge.com/v1/tournaments/${tournamentId}/matches.json?api_key=${server.challongeAPIKey}`)
+            const participantMap = {}
             let b = 0
-            let e = 0
+            let c = 0
+            let d = 0
 
-            for (let i = 0; i < replays.length; i++) {
-                try {
-                    const replay = replays[i]
-                    if (!data[replay.tournamentId]) {
-                        const {data: allMatches} = await axios.get(`https://api.challonge.com/v1/tournaments/${replay.tournamentId}/matches.json?api_key=${server.challongeAPIKey}`).catch((err) => console.log(err))
-                        data[replay.tournamentId] = allMatches
-                    }
-                    
-                    if (Array.isArray(data[replay.tournamentId])) {
-                        const index = data[replay.tournamentId].findIndex((m) => m.match?.id === replay?.match?.challongeMatchId)
-                        if (index >= 0) {
-                            const suggestedOrder = index + 1
-                            await replay.update({ suggestedOrder })
-                            b++
-                        } else {
-                            console.log(`missing: ${replay?.match?.challongeMatchId} from:\n${data[replay.tournamentId]}`)
+            for (let i = 0; i < participants.length; i++) {
+                const { participant } = participants[i]
+                const [discordName,] = participant.name.split('#')
+                const players = [...await Player.findAll({
+                    where: {
+                        [Op.or]: {
+                            discordName: {[Op.iLike]: discordName},
+                            globalName: {[Op.iLike]: discordName}
                         }
                     }
-                } catch (err) {
-                    console.log(err)
-                    e++
+                }), ...[...await Alius.findAll({
+                    where: {
+                        formerName: {[Op.iLike]: discordName}
+                    },
+                    include: Player
+                })].map((a) => a.player)]
+        
+                if (!players.length) {
+                    console.log(`CANNOT FIND PLAYER matching participant: ${participant.name} (${participant.id})`)
+                } else if (players.length > 1) {
+                    const index = prompt(`Found multiple players: ${players.map((p, index) => `${index + 1}. ${p.discordName} (${p.discordId})`).join('\n')}`) - 1
+                    console.log(`selected index:`, index)
+                    participantMap[participant.id] = players[index].dataValues
+                } else {
+                    participantMap[participant.id] = players[0].dataValues
                 }
             }
 
-            return await interaction.editReply(`Fixed ${b} out of ${replays.length} replays. Encounted ${e} errors.`)
+            console.log('participantMap', participantMap)
+
+            if (Object.entries(participantMap).length < tournamentData.tournament.participants_count) {
+                return console.log(`missing ${tournamentData.tournament.participants_count - Object.entries(participantMap).length} partcipants`)
+            }
+
+            for (let i = 0; i < matches.length; i++) {
+                const { match } = matches[i]
+                const retrobotMatch = await Match.findOne({ where: { challongeMatchId: match.id }})
+        
+                if (retrobotMatch) {     
+                    d++  
+                    console.log(`match ${match.id} has already been recorded`)
+                    continue
+                } else if (!retrobotMatch && match.forfeited) {     
+                    c++  
+                    console.log(`match ${match.id} appears to be forfeited from ${tournament.name}`)
+                    continue
+                } else if (!retrobotMatch && !match.forfeited) {
+                    console.log('match.id', match.id, '| match.forfeited', match.forfeited)
+                    const botMatch = await Match.create({
+                        format: 'Goat',
+                        formatId: 8,
+                        challongeMatchId: match.id,
+                        winner: participantMap[match.winner_id].name,
+                        loser: participantMap[match.loser_id].name,
+                        winnerId: participantMap[match.winner_id].id,
+                        loserId: participantMap[match.loser_id].id,
+                        isTournament: true,
+                        serverId: '414551319031054346',
+                        createdAt: match.completed_at
+                    })
+                    b++
+
+                    await botMatch.update({ createdAt: match.completed_at })
+                }
+            }
+    
+            return await interaction.editReply(`Generated new matches for ${b} matches from ${tournament.name}.${d ? ` ${d} matches were already recorded.` : ''}${c ? ` ${c} matches appear to have been forfeited.` : ''} ${b + d + c} out of ${matches.length} matches are now accounted for.`)
         } else {
             return await interaction.editReply('🛠️')
         }
