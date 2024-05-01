080789742600b2c532b63f787028ccf01859c7dc
Daniel McNelis <dwm253@gmail.com>
2024-05-01 15:00:14 -0400

    countries

--- libs/components/src/lib/Profiles/PlayerProfile.jsx
+++ libs/components/src/lib/Profiles/PlayerProfile.jsx
@@ -114,7 +114,7 @@ export const PlayerProfile = () => {
                         <div className="profile-line"><b>Discord:</b> {player.globalName || player.discordName && player.discriminator && player.discriminator !== '0' ? (<><span>{player.discordName}</span><span style={{ color: 'gray' }}>#{player.discriminator}</span></>) : player.discordName || 'N/A'}</div>
                         {
                             player.country ? (
-                                <div className="profile-line"><b>Country:</b> {regionNames.of(iso2ToFips[player.country] || player.country)} <img className="country" src={`https://www.worldometers.info/img/flags/${(player.country).toLowerCase()}-flag.gif`} alt={player.country + ' flag'}/></div>
+                                <div className="profile-line"><b>Country:</b> {regionNames.of(player.country)} <img className="country" src={`https://www.worldometers.info/img/flags/${(player.country).toLowerCase()}-flag.gif`} alt={player.country + ' flag'}/></div>
                             ) : ''
                         }
                         {
