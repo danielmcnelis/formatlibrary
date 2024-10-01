1233ad6f5e5f1185755168d51c96e6038b860bc6
Daniel McNelis <dwm253@gmail.com>
2024-09-30 23:48:51 -0400

    raptive ads

--- apps/formatlibrary/src/app/App.tsx
+++ apps/formatlibrary/src/app/App.tsx
@@ -58,7 +58,7 @@ const App = () => {
             {
                 playerId && !checkedSubscription ? '' : 
                     !isSubscriber ? (
-                        <script data-no-optimize="1" data-cfasync="false" src="raptive.js"></script>
+                        <script data-no-optimize="1" data-cfasync="false" src="https://formatlibrary.com/raptive.js"></script>
                         // <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2048547741313745" crossOrigin="anonymous"></script>
                     ) : ''
             }
