a9f26af155d095eb4633d1765a188026ba1b7eed
Daniel McNelis <dwm253@gmail.com>
2024-10-30 21:56:50 -0400

    replays api

--- libs/components/src/lib/Replays/ReplayTable.jsx
+++ libs/components/src/lib/Replays/ReplayTable.jsx
@@ -111,7 +111,7 @@ export const ReplayTable = () => {
       setPage(1)
       setFormat(null)
       setReplays(replays)
-      setSortBy('publishDate:desc,display:desc,suggestedOrder:desc')
+      setSortBy('publishDate:desc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last')
       setQueryParams({
         player: null,
         event: null,
@@ -257,12 +257,12 @@ export const ReplayTable = () => {
                             style={{width: '230px'}}
                             onChange={(e) => {setSortBy(e.target.value); setPage(1)}}
                         >
-                        <option value="publishDate:desc,display:desc,suggestedOrder:desc">Date: New ⮕ Old</option>
-                        <option value="publishDate:asc,display:desc,suggestedOrder:desc">Date: Old ⮕ New</option>
-                        <option value="winnerName:asc,display:desc,suggestedOrder:desc">Winner: A ⮕ Z</option>
-                        <option value="winnerName:desc,display:desc,suggestedOrder:desc">Winner: Z ⮕ A</option>
-                        <option value="winningDeckType:asc,display:desc,suggestedOrder:desc">Winning Deck: A ⮕ Z</option>
-                        <option value="winningDeckType:desc,display:desc,suggestedOrder:desc">Winning Deck: Z ⮕ A</option>
+                            <option value="publishDate:desc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last">Date: New ⮕ Old</option>
+                            <option value="publishDate:asc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last">Date: Old ⮕ New</option>
+                            <option value="winnerName:asc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last">Winner: A ⮕ Z</option>
+                            <option value="winnerName:desc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last">Winner: Z ⮕ A</option>
+                            <option value="winningDeckType:asc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last">Winning Deck: A ⮕ Z</option>
+                            <option value="winningDeckType:desc,matchId:desc,roundInt:desc,suggestedOrder:desc nulls last">Winning Deck: Z ⮕ A</option>
                         </select>
             
                         <div
--- services/api/src/app/middleware/replays.ts
+++ services/api/src/app/middleware/replays.ts
@@ -45,8 +45,7 @@ export const getReplays = async (req, res, next) => {
         const sort = req.query.sort ? req.query.sort.split(',').reduce((reduced, val) => {
             reduced.push(val.split(':'))
             return reduced
-        }, [['publishDate', 'desc'], ['matchId', 'desc'], ['roundInt', 'desc'], ['suggestedOrder', 'desc nulls last']]) : 
-        [['publishDate', 'desc'], ['matchId', 'desc'], ['roundInt', 'desc'], ['suggestedOrder', 'desc nulls last']]
+        }, []) : [['publishDate', 'desc'], ['matchId', 'desc'], ['roundInt', 'desc'], ['suggestedOrder', 'desc nulls last']]
 
         const replays = await Replay.find(filter, limit, page, sort)
         res.json(replays)
