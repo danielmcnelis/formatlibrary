e819a418dc44ad0d1234fc0f74d7697757f5f89f
Daniel McNelis <dwm253@gmail.com>
2024-11-29 12:45:13 -0500

    api limit block

--- libs/bot-commands/src/lib/username.js
+++ libs/bot-commands/src/lib/username.js
@@ -31,12 +31,20 @@ export default {
         const user = interaction.options.getUser('user') || interaction.user
         const userIsAuthor = user.id === interaction.user.id
         const name = interaction.options.getString('name')
+        console.log('submitted name:', name)
         const [simulator, colName] = interaction.options.getString('simulator').split(', ')
+        console.log('simulator:', simulator)
+        console.log('colName:', colName)
+        console.log('prev. player.duelingBookName:', player.duelingBookName)
+        console.log('prev. player[colName]:', player[colName])
         const player =  await Player.findOne({ where: { discordId: user.id } })
         if (userIsAuthor && name) {
             await player.update({ [colName]: name })
+            console.log('new player.duelingBookName:', player.duelingBookName)
+            console.log('new player[colName]:', player[colName])
             return await interaction.reply({ content: `Your ${simulator} username has been set to: ${player[colName]}`})
         } else {
+            console.log('not author or no name')
             return await interaction.reply({ content: `${userIsAuthor ? 'Your' : `${user.username}'s`} ${simulator} username is: ${player[colName]}`})
         }
 	}
