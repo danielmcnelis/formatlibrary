d8f152ed9a23a7033d9f22708d973c530b9c52eb
Daniel McNelis <dwm253@gmail.com>
2024-12-03 01:43:55 -0500

    bot debugging

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -18,26 +18,6 @@ export default {
             await interaction.editReply(emojis.yellow)
             // await updateMarketPrices()
             // return await conductCensus(client)
-            const tournament = await Tournament.findOne({ where: { id: '15458395'}})
-            console.log('interaction.guildId', interaction.guildId)
-            const server = await Server.findOne({ where: { id: interaction.guildId }})
-            const { data: matches } = await axios.get(`https://api.challonge.com/v1/tournaments/15390013/matches.json?api_key=${server.challongeApiKey}`)
-            const { data: participants } = await axios.get(`https://api.challonge.com/v1/tournaments/15390013/participants.json?api_key=${server.challongeApiKey}`)
-            const {participant} = participants[5]
-            const standings = await calculateStandings(tournament, matches, participants)                
-            console.log('standings', standings)
-            console.log('participant', participant)
-            console.log('participant.id', participant.id)
-            console.log('participant.id === 247112168', participant.id === 247112168)
-            const standing = standings?.find((s) => s.participantId === 247112168)
-            console.log('standing', standing)
-            console.log('standing && standing.rank', standing && standing.rank)
-            console.log(`parseInt(standing.rank.replace(/^D+/g, ''))`, parseInt(standing?.rank?.replace(/^\D+/g, '')))
-            console.log('participant.final_rank', participant?.final_rank)
-            const placement = standing && standing.rank ? parseInt(standing.rank.replace(/^\D+/g, '')) :
-                participant.final_rank ? parseInt(participant.final_rank) :
-                null
-            return console.log('placement', placement)
             // await updateAvatars(client)
             // await updateDeckThumbs()
             // await updateDeckTypes()
@@ -45,7 +25,7 @@ export default {
             // await updateReplays()
             // await updateMatchups()
             // return updateGlobalNames()
-            // return runNightlyTasks(client)
+            return runNightlyTasks(client)
             // return runMonthlyTasks(client)
         } else {
             await interaction.editReply('🧪')
