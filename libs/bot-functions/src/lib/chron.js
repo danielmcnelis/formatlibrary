8895ef9f3648db8782e4595e3f5cf674ff95533f
Daniel McNelis <dwm253@gmail.com>
2024-12-27 15:10:03 -0500

    ladder recalcs and new k factor of 10

--- libs/bot-functions/src/lib/chron.js
+++ libs/bot-functions/src/lib/chron.js
@@ -641,10 +641,10 @@ export const recalculateStats = async () => {
                 const origEloLoser = loserStats.elo || 500.00
 
                 const winnerKFactor = winnerStats.games < 10 ? 30 :
-                    winnerStats.bestElo < 650 ? 15 : 10
+                    winnerStats.bestElo < 590 ? 15 : 10
 
                 const loserKFactor = loserStats.games < 10 ? 30 :
-                    loserStats.bestElo < 650 ? 15 : 10
+                    loserStats.bestElo < 590 ? 15 : 10
 
                 const winnerDelta = winnerKFactor * (1 - (1 - 1 / ( 1 + (Math.pow(10, ((origEloWinner - origEloLoser) / 400))))))
                 const loserDelta = loserKFactor * (1 - (1 - 1 / ( 1 + (Math.pow(10, ((origEloWinner - origEloLoser) / 400))))))
