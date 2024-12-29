0603c2cdb1a017dec1c1835df4117c9931a7a76b
Daniel McNelis <dwm253@gmail.com>
2024-12-29 02:13:30 -0500

    ladder recalcs and new k factor of 10

--- libs/bot-functions/src/lib/chron.js
+++ libs/bot-functions/src/lib/chron.js
@@ -745,8 +745,8 @@ export const applyDecay = async (format, currentDate, nextDate) => {
 
     const activeGeneralPlayerIds = []
     const activeGeneralPlayerNames = []
-    let generalDecayRate = Math.pow(Math.E, (-1 * generalMatchesInPeriod.length) / 10000)
-    if (generalDecayRate < 0.9997) generalDecayRate = 0.9997
+    let generalDecayRate = Math.pow(Math.E, (-1 * generalMatchesInPeriod.length) / 40000)
+    if (generalDecayRate < 0.99975) generalDecayRate = 0.99975
 
     for (let i = 0; i < generalMatchesInPeriod.length ; i++) {
         const {winnerId, winnerName, loserId, loserName} = generalMatchesInPeriod[i]
