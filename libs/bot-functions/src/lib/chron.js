2b19d011b869d54a3c173dce3e220ecf3e7fe891
Daniel McNelis <dwm253@gmail.com>
2025-01-02 23:03:44 -0500

    faster and more accurate recalcs

--- libs/bot-functions/src/lib/chron.js
+++ libs/bot-functions/src/lib/chron.js
@@ -1716,7 +1716,7 @@ export const downloadNewCards = async () => {
             }
 
             if (!card) {
-                const success = await downloadCardImage(id, name, card.id, true)
+                const success = await downloadCardImage(id, name, id, true)
                 console.log(`Image saved (${name})`)
 
                 await Card.create({
