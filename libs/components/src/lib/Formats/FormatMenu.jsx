4f87f193060655e44f181f97d3f7686df0505bf0
Daniel McNelis <dwm253@gmail.com>
2025-01-08 19:25:04 -0500

    ocg formats

--- libs/components/src/lib/Formats/FormatMenu.jsx
+++ libs/components/src/lib/Formats/FormatMenu.jsx
@@ -61,7 +61,7 @@ export const FormatMenu = (props) => {
                 {
                     // isContentManager ? (
                         <>
-                            <div style={{"position":"fixed", "top":"10px", "right":"100px", "padding":"10px"}}>
+                            <div style={{"position":"fixed", "top":"40px", "right":"30px", "padding":"10px"}}>
                                 <h2>{category}</h2>
                                 <div 
                                     id={`category-toggle-${category}`} 
