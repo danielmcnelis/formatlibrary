e3fb98164c6694eaca29dc0446cae8b072ac5997
Daniel McNelis <dwm253@gmail.com>
2025-01-28 11:28:13 -0500

    status indicator fix

--- libs/bot-functions/src/lib/utility.js
+++ libs/bot-functions/src/lib/utility.js
@@ -172,23 +172,17 @@ export const getCard = async (query, fuzzyCards, format) => {
 
     const firstPrint = print ? `${rarities[print.rarity]} ${print.set.name}` : null 
     const dateType = format?.category?.toLowerCase() + 'Date'
-    console.log('dateType', dateType)
     const legal = format && card[dateType] && (card[dateType] <= format.date || format.name === 'Traditional' || format.name === 'Advanced')
-    console.log('legal', legal)
     const position = format?.name === 'Traditional' && legal && status && status.restriction === 'Forbidden' ? 'limited' :
         legal && status ? status.restriction :
         legal && !status ? 'unlimited' :
         'not legal'
     
-    console.log('position', position)
-
     const indicator = position === 'limited' || position === 'limited-1' ? emojis.limited :
         position === 'semi-limited' || position === 'limited-2' ? emojis.semi :
         position === 'no longer on list' || position === 'unlimited' || position === 'limited-3' ? emojis.unlimited :
         position === 'forbidden' ? emojis.forbidden :
         ''
-
-    console.log('indicator', indicator)
 
 	const color = card.category === "Spell" ? "#42f578" :
 		card.category === "Trap" ? "#e624ed" :
@@ -234,9 +228,8 @@ export const getCard = async (query, fuzzyCards, format) => {
         labels.push(`\nFirst Print: ${firstPrint || (format?.category === 'Speed' ? 'N/A' : 'OCG Only')}`)
     }
 
-    console.log('format?.name', format?.name)
     if (format?.name) {
-        `\n${format?.name} ${format?.emoji} Status: ${capitalize(position, true)} ${indicator}`
+        labels.push(`\n${format?.name} ${format?.emoji} Status: ${capitalize(position, true)} ${indicator}`)
     }
 
     if (card.category === 'Monster') {
@@ -253,8 +246,6 @@ export const getCard = async (query, fuzzyCards, format) => {
             ...labels
         ]
     }
-
-    console.log('labels', labels)
 
 	const stats = card.category === "Monster" ? 
 			`ATK: ${card.atk === null ? '?' : card.atk} ${emojis.ATK}` + 
