fb8b100c320c82ef584e9999dcf2dbfcf4f4c9ad
Daniel McNelis <dwm253@gmail.com>
2025-02-04 09:04:56 -0500

    subscriptions

--- services/api/src/app/middleware/images.ts
+++ services/api/src/app/middleware/images.ts
@@ -3,7 +3,78 @@ import axios from 'axios'
 import { Upload } from '@aws-sdk/lib-storage';
 import { S3 } from '@aws-sdk/client-s3';
 import { config } from '@fl/config'
-import {uploadCardImages} from '@fl/bot-functions'
+
+// DOWNLOAD CROPPED IMAGE AND UPLOAD TO ARTWORK FOLDER
+export const uploadCroppedImage = async (s3, artworkId) => {
+    try {
+        const {data: croppedCardImage} = await axios({
+            method: 'GET',
+            url: `https://images.ygoprodeck.com/images/cards_cropped/${artworkId}.jpg`,
+            responseType: 'stream'
+        })
+        
+        const { Location: artworkUri} = await new Upload({
+            client: s3,
+            params: { Bucket: 'formatlibrary', Key: `images/artworks/${artworkId}.jpg`, Body: croppedCardImage, ContentType: `image/jpg` },
+        }).done()
+        console.log('artwork image uri', artworkUri)
+        return true
+    } catch (err) {
+        console.log(err)
+        return false
+    }
+}
+
+// DOWNLOAD CARD IMAGE, RESIZE, AND UPLOAD TO MEDIUM CARDS FOLDER
+export const uploadMediumCardImage = async (s3, artworkId) => {
+   try {
+        const {data: fullCardImage} = await axios({
+            method: 'GET',
+            url: `https://images.ygoprodeck.com/images/cards/${artworkId}.jpg`,
+            responseType: 'stream'
+        })
+
+        const mediumCardImage = fullCardImage.pipe(sharp().resize(144, 210).jpeg())
+        const { Location: imageUri} = await new Upload({
+            client: s3,
+            params: { Bucket: 'formatlibrary', Key: `images/medium_cards/${artworkId}.jpg`, Body: mediumCardImage, ContentType: `image/jpg` },
+        }).done()
+        console.log('medium card image uri', imageUri)
+        return true
+    } catch (err) {
+        console.log(err)
+        return false
+    }
+}
+
+// DOWNLOAD CARD IMAGE AND UPLOAD TO CARDS FOLDER
+export const uploadFullCardImage = async (s3, artworkId) => {
+    const {data: fullCardImage} = await axios({
+        method: 'GET',
+        url: `https://images.ygoprodeck.com/images/cards/${artworkId}.jpg`,
+        responseType: 'stream'
+    })
+
+    try {
+        const { Location: imageUri} = await new Upload({
+            client: s3,
+            params: { Bucket: 'formatlibrary', Key: `images/cards/${artworkId}.jpg`, Body: fullCardImage, ContentType: `image/jpg` },
+        }).done()
+        console.log('card image uri', imageUri)
+        return true
+    } catch (err) {
+        console.log(err)
+        return false
+    }
+}
+
+// UPLOAD CARD IMAGES
+export const uploadCardImages = async (s3, artworkId) => {
+    const fullSuccess = await uploadFullCardImage(s3, artworkId)
+    const mediumSuccess = await uploadMediumCardImage(s3, artworkId)
+    const croppedSuccess = await uploadCroppedImage(s3, artworkId)
+    return [fullSuccess, mediumSuccess, croppedSuccess]
+}
 
 // IMAGES UPDATE CARD
 export const updateCardImage = async (req, res, next) => {
@@ -16,7 +87,7 @@ export const updateCardImage = async (req, res, next) => {
             }
         })
 
-        const [fullSuccess, mediumSuccess, croppedSuccess] = uploadCardImages(s3, req.query.artworkId)
+        const [fullSuccess, mediumSuccess, croppedSuccess] = await uploadCardImages(s3, req.query.artworkId)
 
         if (fullSuccess && mediumSuccess && croppedSuccess) {
             res.json({success: true})
