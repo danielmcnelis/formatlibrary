7e52f25416b27a05f03e35706175314e7e796f3d
Daniel McNelis <dwm253@gmail.com>
2025-02-07 14:38:53 -0500

    subscriptions

--- libs/bot-commands/src/lib/legal.js
+++ libs/bot-commands/src/lib/legal.js
@@ -13,10 +13,10 @@ export default {
             const server = await Server.findOrCreateByIdOrName(interaction.guildId, interaction.guild?.name)
             const format = await Format.findByServerOrChannelId(server, interaction.channelId)
             if (!format) return await interaction.reply({ content: `Try using **/legal** in channels like: <#414575168174948372> or <#629464112749084673>.`})
-            if (format.category !== 'TCG'  && format.category !== 'OCG' && format.category !== 'Speed') return await interaction.reply(`Sorry, ${format.category} formats are not supported at this time.`)
+            if (format.category !== 'TCG' && format.category !== 'OCG' && format.category !== 'Speed') return await interaction.reply(`Sorry, ${format.category} formats are not supported at this time.`)
             await interaction.reply(`Please check your DMs.`)
             
-            if (format.category === 'TCG' || format.category === 'Highlander') {
+            if (format.category === 'TCG' || format.category === 'OCG' || format.category === 'Highlander') {
                 return await checkDeckList(interaction.member, format)
             } else if (format.category === 'Speed') {
                 return await getSkillCard(interaction.member, format)
