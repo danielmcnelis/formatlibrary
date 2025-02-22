67223108c69fb1ccf7d766b90af925e7368147a7
Daniel McNelis <dwm253@gmail.com>
2025-02-22 15:07:21 -0500

    second rated confirmation

--- libs/bot-commands/src/lib/rated.js
+++ libs/bot-commands/src/lib/rated.js
@@ -24,9 +24,11 @@ const getRatedInformation = async (interaction, player, format) => {
             }, 
             include: Server
         })
-        
+        console.log('server.id', server.id)
         if (!server) return await interaction.user.send(`Sorry, you are not a member of a server that supports rated play for ${format.name} Format. ${format.emoji}`)
         const guild = client.guilds.cache.get(server.id)
+    console.log('guild?.name', guild?.name)
+    console.log('format.channelId',format.channelId)
         const channel = guild.channels.cache.get(format.channelId)
         const simName = player.duelingBookName || await askForSimName(interaction.user, player, 'DuelingBook')
         if (!simName) return
