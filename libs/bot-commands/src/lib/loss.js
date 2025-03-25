38509287e98d8ee1a32483365392f6c706577e83
Daniel McNelis <dwm253@gmail.com>
2025-03-25 11:35:00 -0400

    group stage debug

--- libs/bot-commands/src/lib/loss.js
+++ libs/bot-commands/src/lib/loss.js
@@ -118,7 +118,7 @@ export default {
                 })
             }
 
-            const activeTournament = await Tournament.count({ where: { state: {[Op.includes]: 'underway'}, serverId: interaction.guildId, formatName: {[Op.or]: [format.name, 'Multiple']} }}) 
+            const activeTournament = await Tournament.count({ where: { state: {[Op.iLike]: '%underway%'}, serverId: interaction.guildId, formatName: {[Op.or]: [format.name, 'Multiple']} }}) 
             let isTournament, winningEntry, losingEntry, tournament, match, challongeMatch
 
             if (activeTournament) {
