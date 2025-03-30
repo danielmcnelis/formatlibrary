d96dc57d281ccdfa704472e99ecbabef1b23e3b7
Daniel McNelis <dwm253@gmail.com>
2025-03-30 18:44:17 -0400

    subscriber flair stripe

--- libs/bot-commands/src/lib/flair.js
+++ libs/bot-commands/src/lib/flair.js
@@ -46,7 +46,7 @@ export default {
             const currentRoles = [...(await interaction.member.roles.cache.filter((r) => r.name.includes('(Sub)'))).values()]
             const privilegedUserIds = ['194147938786738176', '626843317010694176']
 
-            if (!interaction.member._roles?.includes('1102002844850208810') && !privilegedUserIds.includes(interaction.member.id)) {
+            if (!interaction.member._roles?.includes('1102002844850208810') && !interaction.member._roles?.includes('1336745321186988084') && !privilegedUserIds.includes(interaction.member.id)) {
                 return await interaction.reply({ content: `Sorry, Subscriber Flair is only available with a Format Library ${emojis.FL} subscription.`})
             }
 
