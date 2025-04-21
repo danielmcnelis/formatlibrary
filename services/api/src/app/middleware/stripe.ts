4c8fe3c436a95671173b91fb333004b8d40ea2aa
Daniel McNelis <dwm253@gmail.com>
2025-04-20 22:43:30 -0400

    comment out stripe

--- services/api/src/app/middleware/stripe.ts
+++ services/api/src/app/middleware/stripe.ts
@@ -5,7 +5,7 @@ import stripe from 'stripe'
 import {config} from '@fl/config'
 import { Player, Subscription } from '@fl/models'
 import { Op } from 'sequelize'
-// console.log('config', config)
+console.log('config', config)
 console.log('config.stripe', config.stripe)
 console.log('config.stripe.clientSecret', config.stripe.clientSecret)
 // const Stripe = new stripe(config.stripe.clientSecret)
