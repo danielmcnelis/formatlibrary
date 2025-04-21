2780660aa8425e4ad824543572ca475cf2532ddd
Daniel McNelis <dwm253@gmail.com>
2025-04-20 22:49:06 -0400

    comment out stripe

--- services/api/src/app/middleware/stripe.ts
+++ services/api/src/app/middleware/stripe.ts
@@ -3,7 +3,7 @@
 import stripe from 'stripe'
 import {config} from '@fl/config'
 import { Player, Subscription } from '@fl/models'
-import { Op } from 'sequelize
+import { Op } from 'sequelize'
 const Stripe = new stripe(config.stripe.clientSecret)
 // import {manageSubscriptions} from '@fl/bot-functions'
 // import {Elements} from '@stripe/react-stripe-js'
