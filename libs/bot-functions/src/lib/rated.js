7b7b9e073e64f3ca1da4204cf45415d1c1787236
Daniel McNelis <dwm253@gmail.com>
2025-05-27 22:44:58 -0400

    merchbot compatibility

--- libs/bot-functions/src/lib/rated.js
+++ libs/bot-functions/src/lib/rated.js
@@ -643,7 +643,7 @@ export const getNewRatedDeck = async (user, player, format, deckToReplace) => {
             const { illegalCards, forbiddenCards, limitedCards, semiLimitedCards, unrecognizedCards, zeroCopiesOwned, oneCopyOwned, twoCopiesOwned } = issues
             if (!illegalCards || !forbiddenCards || !limitedCards || !semiLimitedCards || !unrecognizedCards) return false
             
-            if (illegalCards.length || forbiddenCards.length || limitedCards.length || semiLimitedCards.length) {
+            if (illegalCards.length || forbiddenCards.length || limitedCards.length || semiLimitedCards.length || zeroCopiesOwned.length || oneCopyOwned.length || twoCopiesOwned.length) {
                 let response = [`I'm sorry, ${user.username}, your deck is not legal. ${emojis.mad}`]
                 if (illegalCards.length) response = [...response, `\nThe following cards are not included in this format:`, ...illegalCards]
                 if (forbiddenCards.length) response = [...response, `\nThe following cards are forbidden:`, ...forbiddenCards]
