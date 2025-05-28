62df21534eae2fa13798700e8e8d351f4352a088
Daniel McNelis <dwm253@gmail.com>
2025-05-27 21:57:24 -0400

    merchbot compatibility

--- libs/bot-functions/src/lib/forged.js
+++ libs/bot-functions/src/lib/forged.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { Card, ForgedInventory, ForgedPrint, Status } from "@fl/models"
 import { Op } from "sequelize"
-import emojis from '@fl/bot-emojis'
+import { emojis } from '@fl/bot-emojis'
 import { convertArrayToObject } from "./utility"
 
 // GET FORGED ISSUES
