ee6b0a82405d2a0454bb4dffa6e58a9787ef9a0b
Daniel McNelis <dwm253@gmail.com>
2025-05-27 22:20:38 -0400

    merchbot compatibility

--- libs/bot-functions/src/lib/forged.js
+++ libs/bot-functions/src/lib/forged.js
@@ -157,7 +157,7 @@ export const getForgedDeckList = async (member, player, format, override = false
             if (format.category !== 'TCG' && format.category !== 'OCG' && format.category !== 'Speed' && format.name !== 'Forged in Chaos') {
                 member.send({ content: `Thanks, ${member.user.username}, ${pronoun} deck has been saved. ${emojis.legend}\n\nPlease note: Decks for ${format.category} Formats cannot be verified at this time. Be sure your deck is legal for this tournament!`}).catch((err) => console.log(err))
                 return { url, ydk }
-            } else if (illegalCards.length || forbiddenCards.length || limitedCards.length || semiLimitedCards.length) {
+            } else if (illegalCards.length || forbiddenCards.length || limitedCards.length || semiLimitedCards.length || zeroCopiesOwned.length || oneCopyOwned.length || twoCopiesOwned.length) {
                 let response = [`I'm sorry, ${member.user.username}, your deck is not legal. ${emojis.mad}`]
                 if (illegalCards.length) response = [...response, `\nThe following cards are not included in this format:`, ...illegalCards]
                 if (forbiddenCards.length) response = [...response, `\nThe following cards are forbidden:`, ...forbiddenCards]
