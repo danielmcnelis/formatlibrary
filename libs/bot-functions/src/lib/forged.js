8f17966ad97d7c25e80c518f3decc9a2279a0567
Daniel McNelis <dwm253@gmail.com>
2025-05-27 22:06:16 -0400

    merchbot compatibility

--- libs/bot-functions/src/lib/forged.js
+++ libs/bot-functions/src/lib/forged.js
@@ -157,7 +157,7 @@ export const getForgedDeckList = async (member, player, format, override = false
             const { illegalCards, forbiddenCards, limitedCards, semiLimitedCards, unrecognizedCards, zeroCopiesOwned, oneCopyOwned, twoCopiesOwned } = issues
             if (!illegalCards || !forbiddenCards || !limitedCards || !semiLimitedCards || !unrecognizedCards || !zeroCopiesOwned || !oneCopyOwned || !twoCopiesOwned) return false
             
-            if (format.category !== 'TCG' && format.category !== 'OCG' && format.category !== 'Speed') {
+            if (format.category !== 'TCG' && format.category !== 'OCG' && format.category !== 'Speed' && format.name !== 'Forged in Chaos') {
                 member.send({ content: `Thanks, ${member.user.username}, ${pronoun} deck has been saved. ${emojis.legend}\n\nPlease note: Decks for ${format.category} Formats cannot be verified at this time. Be sure your deck is legal for this tournament!`}).catch((err) => console.log(err))
                 return { url, ydk }
             } else if (illegalCards.length || forbiddenCards.length || limitedCards.length || semiLimitedCards.length) {
@@ -186,7 +186,7 @@ export const getForgedDeckList = async (member, player, format, override = false
                 
                 member.send({ content: response.toString() }).catch((err) => console.log(err))
                 return false
-                } else {
+            } else {
                 member.send({ content: `Thanks, ${member.user.username}, ${pronoun} deck is perfectly legal. ${emojis.legend}`}).catch((err) => console.log(err))
                 return { url, ydk }
             }
