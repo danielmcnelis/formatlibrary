c4c8f1db5d0d40df2b245f1948a4a29731bee23f
Daniel McNelis <dwm253@gmail.com>
2025-05-28 15:42:32 -0400

    merchbot compatibility

--- libs/bot-commands/src/lib/loss.js
+++ libs/bot-commands/src/lib/loss.js
@@ -213,7 +213,7 @@ export default {
                 if (format.name === 'Forged in Chaos') {
                     const winnersWallet = await Wallet.findOne({ where: { playerId: winningPlayer.id }})
                     const losersWallet = await Wallet.findOne({ where: { playerId: losingPlayer.id }})
-                    chipsWinner = (Math.round((classicDelta + 2))) < 5 ? 5 : (Math.round((classicDelta + 2))) > 20 ? 20 : (Math.round((classicDelta + 2)))
+                    chipsWinner = (Math.round((classicDelta))) < 5 ? 5 : (Math.round((classicDelta))) > 20 ? 20 : (Math.round((classicDelta )))
                     chipsLoser = (origStatsLoser - origStatsWinner) < 72 ? 5 : (origStatsLoser - origStatsWinner) >=150 ? 3 : 4
                     
                     console.log('chipsWinner', chipsWinner)
