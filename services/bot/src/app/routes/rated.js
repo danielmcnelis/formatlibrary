e2134f837fecb0a9ac5d1ab090ba5130a3c5da09
Daniel McNelis <dwm253@gmail.com>
2025-07-30 13:14:41 -0400

    rated

--- services/bot/src/app/routes/rated.js
+++ services/bot/src/app/routes/rated.js
@@ -4,6 +4,6 @@ import { authenticate, joinRatedPool } from '../middleware'
 
 const router = Router()
 
-router.post('/bot/rated/join', joinRatedPool)
+// router.post('/bot/rated/join', joinRatedPool)
 
 export default router
