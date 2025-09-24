2b8321cd478593ed6a72855ddd55e9512449aef8
Daniel McNelis <dwm253@gmail.com>
2025-09-24 18:51:30 -0400

    genesys

--- services/api/src/app/middleware/points.ts
+++ services/api/src/app/middleware/points.ts
@@ -9,7 +9,7 @@ export const getPoints = async (req, res, next) => {
                 genesysPoints: {[Op.gt]: 0}
             },
             attributes: ['id', 'name', 'artworkId', 'cleanName', 'genesysPoints', 'sortPriority'],
-            order: [['sortPriority', 'ASC'], ['points', 'ASC']]
+            order: [['sortPriority', 'ASC'], ['genesysPoints', 'ASC']]
         })
 
         return res.json(cards)
