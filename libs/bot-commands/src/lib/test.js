e50b73cedd5ada5fa262e476e0bc3527fe699744
Daniel McNelis <dwm253@gmail.com>
2026-01-23 13:19:00 -0500

    subscriber roles fix

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -69,7 +69,7 @@ export default {
                     const player = subscriber.player
                     const tier = subscriber.tier
                     const member = members.find((m) => m.id === player.discordId)
-                    if (tier = 'Premium') {
+                    if (tier === 'Premium') {
                         member.roles.add('1335316985097093290')
                         member.roles.add('1336745321186988084')
                         console.log('added premium/stripe roles back to', member.user.username)
