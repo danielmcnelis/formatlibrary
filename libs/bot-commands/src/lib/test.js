61d978c0cf8a7ecaa49a94c660781882b250a5e5
Daniel McNelis <dwm253@gmail.com>
2026-08-25 16:46:43 -0400

    improving stat recalc efficiency

--- libs/bot-commands/src/lib/test.js
+++ libs/bot-commands/src/lib/test.js
@@ -30,7 +30,7 @@ export default {
                 // const format = await Format.findOne({ where: { name: 'Forged in Chaos' }})
                 // await downloadNewCards(client)
                 // await manageSubscriptions(client)
-                await updateBlogPosts()
+                updateBlogPosts()
                 // await runNightlyTasks(client)
                 // await updateCardLegality()
                 // await removeObsoleteArtworks()
